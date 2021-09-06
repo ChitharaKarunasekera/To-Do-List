4a4d45273836d70c898c7016db49e1f1949afff5
Chithara <chithara.2019@iit.ac.lk>
2021-09-06 12:10:51 +0530

    updated files

--- base/urls.py
+++ base/urls.py
@@ -4,14 +4,14 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     # paths for login logout and registration
-    path('login/', CustomLoginView.as_view(), name='login'),  # Login page
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # Log-out page
+    path('login/', CustomLoginView.as_view(), name='login'),  # login page
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # log-out page
     path('register', RegisterPage.as_view(), name='register'),  # register page
 
     # paths for tasks
-    path('', TaskList.as_view(), name='tasks'),  # List of tasks
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),  # Detailed Task
-    path('task-create/', TaskCreate.as_view(), name='tasks-create'),  # Create Tasks
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),  # Update Task
-    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),  # Delete Task
+    path('', TaskList.as_view(), name='tasks'),  # list of tasks
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),  # detailed task
+    path('task-create/', TaskCreate.as_view(), name='tasks-create'),  # create tasks
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),  # update task
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),  # delete task
 ]
